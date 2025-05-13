0973d896bf17572f16080f19d1279e81a2444e1e
mariamunozlopez <maria.munoz495@comunidadunir.net>
2025-05-13 19:53:30 +0200

    Primer commit: crear componente con dependencias Sprint

---
+++ src/main/java/com/smartcity/parking/PK4U/Pk4UApplication.java
@@ -0,0 +1,13 @@
+package com.smartcity.parking.PK4U;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pk4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pk4UApplication.class, args);
+	}
+
+}
